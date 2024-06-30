e5321b394cc4f2e7929bc9d7b8e8422fc108680c
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-07-01 01:35:27 +0900

    コンテンツ部分を作成し、スタイリング

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home: React.FC = () => {
   return(
     <div className="relative">
-      <header className="line-height:50px; fixed top-0 left-0 right-0">
+      <header className="bg-white line-height:50px; fixed top-0 left-0 right-0">
         <div className="container mx-auto flex justify-between">
           <p className="logo">スケジュール管理APP</p>
           <nav>
